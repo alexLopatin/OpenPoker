0733438ae48b7e3105ef281bb2d14f88e49a8554
alexLopatin <minelabory@gmail.com>
2020-02-18 19:33:37 +0300

    enhanced roleInitializer class

--- OpenPoker/Infrastructure/RoleInitializer.cs
+++ OpenPoker/Infrastructure/RoleInitializer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
 using OpenPoker.Models;
+using System.Xml;
 
 namespace OpenPoker.Infrastructure
 {
@@ -11,20 +12,51 @@ namespace OpenPoker.Infrastructure
     {
         public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
         {
-            string adminEmail = "admin@gmail.com";
-            string password = "123456aA";
-            if (await roleManager.FindByNameAsync("admin") == null)
+            XmlDocument settings = new XmlDocument();
+            settings.Load("IdentityInit.xml");
+            XmlElement xRoot = settings.DocumentElement;
+            if (xRoot.Name == "Identity")
             {
-                await roleManager.CreateAsync(new IdentityRole("admin"));
-            }
-            if (await userManager.FindByNameAsync(adminEmail) == null)
-            {
-                User admin = new User { Email = adminEmail, UserName = adminEmail };
-                userManager.Options.Password.RequireNonAlphanumeric = false;
-                IdentityResult result = await userManager.CreateAsync(admin, password);
-                if (result.Succeeded)
+                foreach (XmlNode xnode in xRoot)
                 {
-                    await userManager.AddToRoleAsync(admin, "admin");
+                    if (xnode.Name == "Roles")
+                    {
+                        foreach (XmlNode roleNode in xnode)
+                        {
+                            string name = roleNode.Attributes["name"].Value;
+                            if (await roleManager.FindByNameAsync(name) == null)
+                            {
+                                await roleManager.CreateAsync(new IdentityRole(name));
+                            }
+                        }
+                    }
+                    if (xnode.Name == "Users")
+                    {
+                        foreach (XmlNode userNode in xnode)
+                        {
+                            string email = userNode.Attributes["email"].Value;
+                            string password = userNode.Attributes["password"].Value;
+                            List<string> roles = new List<string>();
+                            foreach(XmlNode roleNode in userNode)
+                            {
+                                if (roleNode.Name == "User.Role")
+                                    roles.Add(roleNode.Attributes["name"].Value);
+                            }
+                            if (await userManager.FindByNameAsync(email) == null)
+                            {
+                                User admin = new User { Email = email, UserName = email };
+                                userManager.Options.Password.RequireNonAlphanumeric = false;
+                                IdentityResult result = await userManager.CreateAsync(admin, password);
+                            }
+                            var user = await userManager.FindByNameAsync(email);
+                            if(user != null)
+                                foreach (string role in roles)
+                                {
+                                    if (await userManager.IsInRoleAsync(user, role) == false)
+                                        await userManager.AddToRoleAsync(user, "admin");
+                                }
+                        }
+                    }
                 }
             }
         }
